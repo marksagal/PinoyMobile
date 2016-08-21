d4a52e07348755b81444165410b39053a4512fd4
Mark Sagal <admin@pinoycentral.net>
2016-08-21 15:29:38 +0800

    Added Controller Home.php as default page

---
+++ application/controllers/Home.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('ERROR');
+class Home extends CI_Controller {
+  public function __construct() {
+    parent::__construct();
+  }
+
+  public function index() {
+
+  }
+}
